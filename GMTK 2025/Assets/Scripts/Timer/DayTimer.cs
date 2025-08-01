154b50ed9a1dc9fa4727250ead7bd942c898ee46
wesleytdavies <wtd224@nyu.edu>
2025-07-31 19:35:26 -0700

    made DayTimer UI and prefab

--- GMTK 2025/Assets/Scripts/Timer/DayTimer.cs
+++ GMTK 2025/Assets/Scripts/Timer/DayTimer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LostResort.SignalShuttles;
+using UnityEngine.UI;
 
 namespace LostResort.Timers
 {
@@ -19,11 +20,19 @@ namespace LostResort.Timers
         public float TimeElapsed { get; private set; }
         
         /// <summary>
+        /// How much time has elapsed in the game in float between 0f and 1f.
+        /// </summary>
+        public float TimeElapsedPercent { get; private set; }
+        
+        /// <summary>
         /// How much time has elapsed in the game in int.
         /// </summary>
         public int TimeElapsedInt { get; private set; }
     
         private IEnumerator _gameTimer;
+
+        [SerializeField] private Image _timerCircle;
+        [SerializeField] private Image[] _hourDots;
         
         void Awake()
         {
@@ -40,12 +49,29 @@ namespace LostResort.Timers
         private IEnumerator GameTimer(float maxTime)
         {
             //Debug.Log("Start timer.");
+            TimeElapsed = 0f;
+            TimeElapsedInt = 0;
+            TimeElapsedPercent = 0f;
             //tracks when a second ticks up
             int lastSecond = 0;
-            while (TimeElapsed<=maxTime)
+            //how many hours have passed. number of total hours is determined by _hourDots.Length
+            int hoursElapsed = 0;
+            while (TimeElapsed <= maxTime)
             {
                 TimeElapsed += Time.deltaTime;
                 TimeElapsedInt = (int)Mathf.Floor(TimeElapsed);
+                TimeElapsedPercent = Mathf.Clamp01(TimeElapsed / maxTime);
+                
+                //update UI
+                _timerCircle.fillAmount = Mathf.Lerp(1f, 0f, TimeElapsed / maxTime);
+                if (TimeElapsedPercent > ((float)hoursElapsed + 1) / _hourDots.Length)
+                {
+                    if (hoursElapsed < _hourDots.Length)
+                    {
+                        _hourDots[hoursElapsed].enabled = false;
+                    }
+                    hoursElapsed++;
+                }
                 
                 if (TimeElapsedInt > lastSecond)
                 {
@@ -75,16 +101,34 @@ namespace LostResort.Timers
         void RegisterSignals()
         {
             SignalShuttle<OnGameStart>.Register(StartTimer);
+            SignalShuttle<OnGameEnd>.Register(ClearUI);
         }
         
         void DeregisterSignals()
         {
             SignalShuttle<OnGameStart>.Deregister(StartTimer);
+            SignalShuttle<OnGameEnd>.Deregister(ClearUI);
         }
     
         void AssignComponents()
         {
             _gameTimer = GameTimer(_gameDuration);
+        }
+
+        void ClearUI(OnGameEnd signal)
+        {
+            if (_timerCircle.enabled)
+            {
+                _timerCircle.enabled = false;
+            }
+
+            foreach (Image hourDot in _hourDots)
+            {
+                if (hourDot.enabled)
+                {
+                    hourDot.enabled = false;
+                }
+            }
         }
     }
 
