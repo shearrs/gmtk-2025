9bc4a649b62dcad9d8271ba62269c1d57adc1a15
wesleytdavies <wtd224@nyu.edu>
2025-08-02 01:10:44 -0700

    connected speed to sand particles

--- GMTK 2025/Assets/Scripts/ParticleSystems/ObjectLanding.cs
+++ GMTK 2025/Assets/Scripts/ParticleSystems/ObjectLanding.cs
@@ -1,25 +1,35 @@
 using System;
 using UnityEngine;
+using LostResort.Cars;
 
 namespace LostResort.ParticleSystems
 {
     public class ObjectLanding : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
+        [SerializeField] private ParticleSystem[] _impactLineParticles;
+        [SerializeField] private ParticleSystem _rubble;
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        [SerializeField] private Wheel[] _wheels;
+        [SerializeField] private Car _car;
 
-        private void OnCollisionEnter(Collision other)
+        private bool _isCarGrounded = false;
+
+        /// <summary>
+        /// x is particle speed at car speed 0, y is particle speed at car speed 1
+        /// </summary>
+        [SerializeField] private Vector2 _speedRange;
+
+        private float _carSpeed = 0f;
+
+        private void Update()
         {
             
+            
+            for (int i = 0; i < _impactLineParticles.Length; i++)
+            {
+                var mainModule = _impactLineParticles[i].main;
+                mainModule.startSpeedMultiplier = Mathf.Lerp(_speedRange.x, _speedRange.y, _carSpeed);
+            }
         }
     }
 }
--- GMTK 2025/Assets/Scripts/ParticleSystems/SandParticles.cs
+++ GMTK 2025/Assets/Scripts/ParticleSystems/SandParticles.cs
@@ -1,26 +1,94 @@
 using System;
 using UnityEngine;
+using LostResort.Cars;
 
 namespace LostResort.ParticleSystems
 {
     public class SandParticles : MonoBehaviour
-    {
-        [SerializeField] private ParticleSystem[] _exhaustParticles;
+    { 
+        [SerializeField] private ParticleSystem[] _sandParticles;
 
         /// <summary>
-        /// x is particle speed at car speed 0, y is particle speed at car speed 1
+        /// x is particle emission  at car speed 0, y is particle emission at car speed 1
+        /// </summary>
+        [SerializeField] private Vector2 _emissionRange;
+        
+        /// <summary>
+        /// x is particle speed  at car speed 0, y is particle speed at car speed 1
         /// </summary>
         [SerializeField] private Vector2 _speedRange;
 
+        [SerializeField] private Car _car;
+
         private float _carSpeed = 0f;
+
+        private bool _isTouchingSand = false;
+
+        [SerializeField] private Collider _sandCollider;
 
         private void Update()
         {
-            for (int i = 0; i < _exhaustParticles.Length; i++)
+            if (_car.GetMaxSpeedPercentage() >= 0.02f) //TODO: get rid of magic number
             {
-                var mainModule = _exhaustParticles[i].main;
-                mainModule.startSpeedMultiplier = Mathf.Lerp(_speedRange.x, _speedRange.y, _carSpeed);
+                for (int i = 0; i < _sandParticles.Length; i++)
+                {
+                    var emissionModule = _sandParticles[i].emission;
+                    emissionModule.rateOverTimeMultiplier = Mathf.Lerp(_emissionRange.x, _emissionRange.y, _car.GetMaxSpeedPercentage());
+                }
             }
+            else
+            {
+                for (int i = 0; i < _sandParticles.Length; i++)
+                {
+                    var emissionModule = _sandParticles[i].emission;
+                    emissionModule.rateOverTimeMultiplier = 0f;
+                }
+            }
+            
+            for (int i = 0; i < _sandParticles.Length; i++)
+            {
+                var mainModule = _sandParticles[i].main;
+                mainModule.startSpeedMultiplier = Mathf.Lerp(_speedRange.x, _speedRange.y, _car.GetMaxSpeedPercentage());
+            }
+            
+            // for (int i = 0; i < _sandParticles.Length; i++)
+            // {
+            //     var emissionModule = _sandParticles[i].emission;
+            //     emissionModule.rateOverTimeMultiplier = Mathf.Lerp(_emissionRange.x, _emissionRange.y, _car.GetMaxSpeedPercentage());
+            // }
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            Debug.Log("penis");
+            if (other.collider != _sandCollider)
+            {
+                return;
+            }
+
+            if (_isTouchingSand)
+            {
+                return;
+            }
+
+            _isTouchingSand = true;
+            Debug.Log("penis");
+        }
+        
+        private void OnCollisionExit(Collision other)
+        {
+            if (other.collider != _sandCollider)
+            {
+                return;
+            }
+
+            if (!_isTouchingSand)
+            {
+                return;
+            }
+
+            _isTouchingSand = false;
+            Debug.Log("sinep");
         }
     }
 }
