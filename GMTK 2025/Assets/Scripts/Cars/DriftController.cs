b6c0d1ba9efd3981a5c8276430cb75930c41371c
reagancheatham <reagancheatham@gmail.com>
2025-08-01 21:17:28 -0500

    Add mario kart drifting

--- GMTK 2025/Assets/Scripts/Cars/DriftController.cs
+++ GMTK 2025/Assets/Scripts/Cars/DriftController.cs
@@ -7,6 +7,7 @@ namespace LostResort.Cars
     {
         [Header("References")]
         [SerializeField] private Car car;
+        [SerializeField] private WheelController wheelController;
         [SerializeField] private Wheel[] gripDriftWheels;
         [SerializeField] private Wheel[] slipDriftWheels;
         [SerializeField] private Transform chassis;
@@ -14,12 +15,18 @@ namespace LostResort.Cars
         [Header("Drift Settings")]
         [SerializeField] private float defaultGrip = 1.0f;
         [SerializeField] private float driftGrip = 1.0f;
-        [SerializeField] private float driftSlip = 0.2f;
+        [SerializeField] private float minDriftSlip = 0.15f;
+        [SerializeField] private float maxDriftSlip = 0.35f;
+        [SerializeField] private float minDriftSteeringAngle = 10.0f;
+        [SerializeField] private float maxDriftSteeringAngle = 20.0f;
 
         [Header("Chassis Settings")]
         [SerializeField] private float normalChassisRotation = 10.0f;
         [SerializeField] private float driftChassisRotation = 30.0f;
         [SerializeField] private float chassisRotationSpeed = 5.0f;
+
+        private float moveInputDirection;
+        private bool isDrifting = false;
 
         private void OnEnable()
         {
@@ -45,32 +52,59 @@ namespace LostResort.Cars
         private void Update()
         {
             UpdateChassisRotation();
+
+            if (isDrifting)
+                UpdateDrift();
         }
 
         private void OnDriftInputBegin(ManagedInputInfo info)
         {
+            moveInputDirection = car.Input.MoveInput.ReadValue<Vector2>().x;
+
+            if (Mathf.Abs(moveInputDirection) < 0.01f)
+                return;
+
+            moveInputDirection = Mathf.Sign(moveInputDirection);
+            isDrifting = true;
+            wheelController.LockWheelRotationForDrifting(minDriftSteeringAngle, maxDriftSteeringAngle);
+
             foreach (var wheel in gripDriftWheels)
                 wheel.Grip = driftGrip;
+
+            foreach (var wheel in slipDriftWheels)
+                wheel.Grip = minDriftSlip;
+        }
+
+        private void OnDriftInputEnd(ManagedInputInfo info)
+        {
+            if (!isDrifting)
+                return;
+
+            isDrifting = false;
+            moveInputDirection = 0.0f;
+            wheelController.UnlockWheelRotation();
+
+            foreach (var wheel in gripDriftWheels)
+                wheel.Grip = defaultGrip;
+
+            foreach (var wheel in slipDriftWheels)
+                wheel.Grip = defaultGrip;
+        }
+
+        private void UpdateDrift()
+        {
+            float currentInput = car.Input.MoveInput.ReadValue<Vector2>().x;
+            float t = (2.0f * (moveInputDirection - currentInput)) - 1.0f;
+            float driftSlip = Mathf.Lerp(minDriftSlip, maxDriftSlip, t);
 
             foreach (var wheel in slipDriftWheels)
                 wheel.Grip = driftSlip;
         }
 
-        private void OnDriftInputEnd(ManagedInputInfo info)
-        {
-            foreach (var wheel in gripDriftWheels)
-                wheel.Grip = defaultGrip;
-
-            foreach (var wheel in slipDriftWheels)
-                wheel.Grip = defaultGrip;
-        }
-
         private void UpdateChassisRotation()
         {
-            var moveInput = car.Input.MoveInput.ReadValue<Vector2>().x;
-
-            float maxRotation = car.Input.DriftInput.IsPressed() ? driftChassisRotation : normalChassisRotation;
-            maxRotation *= car.GetMaxSpeedPercentage() * moveInput;
+            float maxRotation = isDrifting ? driftChassisRotation : normalChassisRotation;
+            maxRotation *= car.GetMaxSpeedPercentage() * moveInputDirection;
 
             Vector3 rotation = chassis.localEulerAngles;
             float angle = rotation.z;
--- GMTK 2025/Assets/Scripts/Cars/WheelController.cs
+++ GMTK 2025/Assets/Scripts/Cars/WheelController.cs
@@ -17,6 +17,9 @@ namespace LostResort.Cars
         [SerializeField] private float maxSpeedSteeringAngle = 10f;
 
         private Vector3 rotationInput;
+        private bool isLocked = false;
+        private float lockedRotationInput;
+        private Vector2 lockedRotationRange;
 
         private void Update()
         {
@@ -28,16 +31,51 @@ namespace LostResort.Cars
             UpdateRotation();
         }
 
+        public void LockWheelRotationForDrifting(float minSteeringAngle, float maxSteeringAngle)
+        {
+            if (isLocked)
+                return;
+
+            float rotDirection = rotationInput.x;
+            lockedRotationRange = new(rotDirection * minSteeringAngle, rotDirection * maxSteeringAngle);
+            lockedRotationInput = Mathf.Sign(rotDirection);
+
+            isLocked = true;
+        }
+
+        public void UnlockWheelRotation()
+        {
+            isLocked = false;
+        }
+
         private void UpdateRotation()
         {
+            if (isLocked)
+            {
+                foreach (var wheel in wheels)
+                {
+                    Vector3 euler = wheel.transform.localEulerAngles;
+
+                    float inputT = 1.0f - (0.5f * Mathf.Abs(lockedRotationInput - rotationInput.x));
+                    float steerAngle = Mathf.Lerp(lockedRotationRange.x, lockedRotationRange.y, inputT);
+
+                    euler.y = Mathf.MoveTowardsAngle(euler.y, steerAngle, handling * Time.fixedDeltaTime);
+
+                    wheel.transform.localEulerAngles = euler;
+                }
+
+                return;
+            }
+
             foreach (var wheel in wheels)
             {
                 Vector3 euler = wheel.transform.localEulerAngles;
 
                 float velocityT = car.GetMaxSpeedPercentage();
                 float maxSteerAngle = Mathf.Lerp(maxSteeringAngle, maxSpeedSteeringAngle, velocityT);
-                float targetRot = rotationInput.x * maxSteerAngle;
+
                 float currentHandling = handling;
+                float targetRot = maxSteerAngle * rotationInput.x;
 
                 euler.y = Mathf.MoveTowardsAngle(euler.y, targetRot, currentHandling * Time.fixedDeltaTime);
 
