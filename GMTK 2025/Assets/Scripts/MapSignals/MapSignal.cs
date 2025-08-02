13c5961262f1163ab19230e74d38c31eefdbe415
wesleytdavies <wtd224@nyu.edu>
2025-08-02 10:31:59 -0700

    added dynamic music

--- GMTK 2025/Assets/Scripts/MapSignals/MapSignal.cs
+++ GMTK 2025/Assets/Scripts/MapSignals/MapSignal.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using LostResort.SignalShuttles;
 
@@ -6,7 +7,7 @@ public struct RegionChange : ISignal
 {
     public enum Regions
     {
-        Hotel, ConferenceCenter, Beach, Gym 
+        Hotel, Beach, Gym
     }
     
     public Regions NewRegion { get; private set; }
@@ -24,29 +25,43 @@ namespace LostResort.MapSignals
         /// <summary>
         /// Triggers must be indexed in the same order corresponding to RegionChange.Regions
         /// </summary>
-        [SerializeField] private Collider[] _regionEntryTriggers;
+        [SerializeField] private List<GameObject> _hotelEntryTriggers;
+        
+        [SerializeField] private List<GameObject> _beachEntryTriggers;
+        
+        [SerializeField] private List<GameObject> _gymEntryTriggers;
+
+        private List<GameObject>[] _regionEntryTriggersArray;
 
         private RegionChange.Regions _currentRegion;
 
         private void OnEnable()
         {
-            RegisterSignals();
+            //RegisterSignals();
+            MakeArray();
+        }
+
+        private void Start()
+        {
+            //TODO: comment this if you switch to using OnGameStart
+            _currentRegion = RegionChange.Regions.Hotel;
+            SignalShuttle.Emit(new RegionChange(_currentRegion));
         }
 
         private void OnDisable()
         {
-            DeregisterSignals();
+            //DeregisterSignals();
         }
 
         private void OnTriggerEnter(Collider other)
         {
             //Check if trigger was a region entry trigger
-            if(!Array.Exists(_regionEntryTriggers, x => x.gameObject == other.gameObject))
+            if(!Array.Exists(_regionEntryTriggersArray, x => x.Contains(other.gameObject)))
             {
                 return;
             }
             
-            int index = Array.FindIndex(_regionEntryTriggers, x => x.gameObject == other.gameObject);
+            int index = Array.FindIndex(_regionEntryTriggersArray, x => x.Contains(other.gameObject));
 
             if ((int)_currentRegion == index)
             {
@@ -57,20 +72,31 @@ namespace LostResort.MapSignals
             SignalShuttle.Emit(new RegionChange(_currentRegion));
         }
         
-        void OnGameStart(OnGameStart signal)
-        {
-            _currentRegion = RegionChange.Regions.Hotel;
-            SignalShuttle.Emit(new RegionChange(_currentRegion));
-        }
-
-        void RegisterSignals()
-        {
-            SignalShuttle.Register<OnGameStart>(OnGameStart);
-        }
+        //TODO: uncomment to use OnGameStart instead of OnEnable
         
-        void DeregisterSignals()
+        // void OnGameStart(OnGameStart signal)
+        // {
+        //     _currentRegion = RegionChange.Regions.Hotel;
+        //     SignalShuttle.Emit(new RegionChange(_currentRegion));
+        // }
+
+        // void RegisterSignals()
+        // {
+        //     SignalShuttle.Register<OnGameStart>(OnGameStart);
+        // }
+        
+        // void DeregisterSignals()
+        // {
+        //     SignalShuttle.Deregister<OnGameStart>(OnGameStart);
+        // }
+
+        void MakeArray()
         {
-            SignalShuttle.Deregister<OnGameStart>(OnGameStart);
+            //TODO: this is terribly organized
+            _regionEntryTriggersArray = new List<GameObject>[3];
+            _regionEntryTriggersArray[0] = _hotelEntryTriggers;
+            _regionEntryTriggersArray[1] = _beachEntryTriggers;
+            _regionEntryTriggersArray[2] = _gymEntryTriggers;
         }
     }
 }
